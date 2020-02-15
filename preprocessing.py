6820adb11a4036c5385dbb52534800fef9c0f2fa
gontxomde <19506899+gontxomde@users.noreply.github.com>
2020-02-15 20:09:25 +0100

    Modified  packages needed

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,9 @@
 import pandas as pd
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 import math, nltk, warnings
 from nltk.corpus import wordnet
-from sklearn import linear_model
-from sklearn.neighbors import NearestNeighbors
 from fuzzywuzzy import fuzz
-from wordcloud import WordCloud, STOPWORDS
 
 from common import load_tmdb_credits
 from common import load_tmdb_movies
